07840ee88819d83b005f321511f7619b249be062
Sanya Coder <clashoffclans210@gmail.com>
2022-01-06 09:34:35 +0200

    second commit

---
+++ tests/Feature/CompanyControllerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CompanyControllerTest extends TestCase
+{
+//    use RefreshDatabase;
+//    /**
+//     * A basic feature test example.
+//     *
+//     * @return void
+//     */
+//        private $jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3RcL2FwaVwvYXV0aFwvbG9naW4iLCJpYXQiOjE2NDE0MDQ1MzEsImV4cCI6MTY0MTQwODEzMSwibmJmIjoxNjQxNDA0NTMxLCJqdGkiOiJmZG9acHJZTDBIaFp5UjN6Iiwic3ViIjoxLCJwcnYiOiJjZmU3ZWM5OWEyM2Y0Mzg4ZTdmMWQ1ZmI4NzA4Mzc1Yzg1NGVkYTY0IiwiaWQiOjF9.3SEvjX_-d7fgLeCJWob6Eg1Ya8lXMpx7vE3veD_QDiw";
+
+    public function testGetCompanies()
+    {
+        $response = $this->get('/api/companies');
+
+        $response->assertStatus(200);
+    }
+
+    public function testGetCompany()
+    {
+        $response = $this->get('/api/companies/1');
+
+        $response->assertStatus(200);
+    }
+    public function testCreateCompany()
+    {
+        $response = $this->post('/api/companies', [
+            "email"=>"testcompany@gmail",
+            "password"=>"123",
+            "location"=>"Pushkina 12",
+            "town"=>"Chernigov",
+            "status_of_working"=>"1",
+            "description" => "test lorem ipsum"
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+}
